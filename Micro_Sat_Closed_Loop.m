3a6c77df0b75f628c737706bf140069ca983de87
Omer Eren Can KOCULU <omeren.k@gmail.com>
2018-10-16 17:46:41 +0200

    Q21: Remove all nonlinearities, disturbances, delays and noises in the block diagram of question Q19, and compute an open-loop *state-space representation*.

---
+++ Micro_Sat_Closed_Loop.m
@@ -0,0 +1,8 @@
+%Closed-loop control
+
+%% Q21
+%Remove all nonlinearities, disturbances, delays and noises in the 
+%block diagram of question Q19,and compute an open-loop state-space 
+%representation.
+
+[A3,B3,C3,D3]=linmod('Satellite_Closed_Loop_Modal_Control');
